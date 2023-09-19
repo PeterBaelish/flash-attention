ca0ce91592bc78f3dfdfc3b1c703d2770a6992fe
PeterBaelish <baelish104543xx@gmail.com>
2023-09-19 14:28:05 +0800

    fix bug

--- csrc/flash_attn/src/flash_fwd_launch_template.h
+++ csrc/flash_attn/src/flash_fwd_launch_template.h
@@ -17,6 +17,11 @@ __global__ void flash_fwd_kernel(Flash_fwd_params params) {
 
 template<typename Kernel_traits, bool Is_dropout, bool Is_causal, bool Is_even_N, bool Is_even_K, bool Return_softmax>
 __global__ void flash_fwd_kernel_casual(Flash_fwd_params params) {
+    int tidx = threadIdx.x;
+    // The global block index.
+    int block_id = blockIdx.x + blockIdx.y * gridDim.x + gridDim.x * gridDim.y * blockIdx.z;
+    if(tidx == 0 && block_id == 0)
+        printf("z\n");
     flash::compute_attn_casual<Kernel_traits, Is_dropout, Is_causal, Is_even_N, Is_even_K, Return_softmax>(params);
 }
 
